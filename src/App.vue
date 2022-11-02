819dc8a0cf3d9d26a2de8087a7135862a7439262
Netson <netsoncavina@gmail.com>
2022-11-02 13:40:21 -0300

    desenvolvimento do formulario

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <Formulario />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
+    Formulario,
   },
 });
 </script>
