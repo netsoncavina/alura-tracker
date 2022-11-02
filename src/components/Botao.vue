a8f5b6d831585a073b591c17055da760608d317d
Netson <netsoncavina@gmail.com>
2022-11-02 14:51:37 -0300

    desenvolvimento do componente botao

---
+++ src/components/Botao.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="button" @click="clicado" :disabled="isAtivo">
+    <span class="icon">
+      <i :class="icone"></i>
+    </span>
+    <span class="text">{{ texto }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BotaoVue",
+  emits: ["clicado"],
+  props: {
+    isAtivo: {
+      type: Boolean,
+      required: true,
+    },
+    icone: {
+      type: String,
+      required: true,
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clicado(): void {
+      this.$emit("clicado");
+    },
+  },
+});
+</script>
+
+<style scoped>
+.text {
+  padding-left: 1rem;
+}
+</style>
