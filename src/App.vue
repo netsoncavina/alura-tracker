7158e022abbf38f3923697b59f74f115f156215d
Netson <netsoncavina@gmail.com>
2022-11-02 13:09:20 -0300

    adição do componente ao app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <main class="columns is-gapless is-multiline">
-    <div class="column is-one-quarter"></div>
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
     <div class="column is-three-quarter"></div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import BarraLateral from "./components/BarraLateral.vue";
 export default defineComponent({
   name: "App",
+  components: {
+    BarraLateral,
+  },
 });
 </script>
 
