2e62a9205ed1751d5996f0e059d39a1ae3405bac
Netson <netsoncavina@gmail.com>
2022-11-02 14:42:13 -0300

    adição do emit

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -21,6 +21,7 @@ import { defineComponent } from "vue";
 import Cronometro from "./Cronometro.vue";
 export default defineComponent({
   name: "TemporizadorVue",
+  emits: ["aoTemporizadorFinalizado"],
   components: {
     Cronometro,
   },
@@ -41,6 +42,7 @@ export default defineComponent({
     finalizar() {
       this.cronometroRodando = false;
       clearInterval(this.cronometro);
+      this.$emit("aoTemporizadorFinalizado", this.tempoEmSegundos);
       this.tempoEmSegundos = 0;
     },
   },
