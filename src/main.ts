f9e0596aaef579ac9bd12a26836139f90449b79e
Netson Cavina <netsoncavina@gmail.com>
2024-12-29 07:04:44 -0300

    Integrate Vuex store into main application setup

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import roteador from './roteador';
+import { store, key } from './store';
 
-createApp(App).use(roteador).mount('#app');
+createApp(App).use(roteador).use(store, key).mount('#app');
