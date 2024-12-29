e4727be7f401f35b55319be3e0961e9dd1371fb7
Netson Cavina <netsoncavina@gmail.com>
2024-12-29 06:24:44 -0300

    Refactor App.vue to use router-view and create Tarefas.vue for task management

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,37 @@
 <template>
-  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+  <main
+    class="columns is-gapless is-multiline"
+    :class="{ 'modo-escuro': modoEscuroAtivo }"
+  >
     <div class="column is-one-quarter">
-      <BarraLateral @aoTemaAlterado="trocarTema"/>
+      <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <Box v-if="listaEstaVazia">
-          Você não está muito produtivo hoje :(
-        </Box>
-      </div>
+      <router-view />
     </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue'
-import Formulario from './components/Formulario.vue'
-import Tarefa from './components/Tarefa.vue'
-import Box from './components/Box.vue'
-import ITarefa from './interfaces/ITarefa'
+import BarraLateral from './components/BarraLateral.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box
   },
-  data () {
+  data() {
     return {
-      tarefas: [] as ITarefa[],
-      modoEscuroAtivo: false
-    }
+      modoEscuroAtivo: false,
+    };
   },
-  computed: {
-    listaEstaVazia () : boolean {
-      return this.tarefas.length === 0
-    }
-  },
+
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
     },
-    trocarTema (modoEscuroAtivo: boolean) {
-      this.modoEscuroAtivo = modoEscuroAtivo
-    }
-  }
+  },
 });
 </script>
 
