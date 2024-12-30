368e04312bd0f677e7673741e378be1cdae075a5
Netson Cavina <netsoncavina@gmail.com>
2024-12-30 06:58:38 -0300

    Add task selection and editing functionality: implement modal for editing tasks and emit event on task click

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,8 +1,48 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
-    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :key="index"
+      :tarefa="tarefa"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+
     <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
+  </div>
+  <div
+    class="modal"
+    :class="{ 'is-active': tarefaSelecionada }"
+    v-if="tarefaSelecionada"
+  >
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Editando a tarefa</p>
+        <button class="delete" aria-label="close" @click="fecharModal"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field">
+          <label for="descricaoDaTarefa" class="label">
+            Descrição da tarefa
+          </label>
+          <input
+            type="text"
+            v-model="tarefaSelecionada.descricao"
+            class="input"
+            id="descricaoDaTarefa"
+          />
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <div class="buttons">
+          <button class="button is-success" @click="alterarTarefa">
+            Salvar alterações
+          </button>
+          <button class="button" @click="fecharModal">Cancelar</button>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -14,6 +54,7 @@ import Box from '../components/Box.vue';
 import ITarefa from '../interfaces/ITarefa';
 import { useStore } from '@/store';
 import {
+  ALTERAR_TAREFA,
   CADASTRAR_TAREFA,
   OBTER_PROJETOS,
   OBTER_TAREFAS,
@@ -26,7 +67,11 @@ export default defineComponent({
     Tarefa,
     Box,
   },
-
+  data() {
+    return {
+      tarefaSelecionada: null as ITarefa | null,
+    };
+  },
   computed: {
     listaEstaVazia(): boolean {
       return (this.tarefas as ITarefa[]).length === 0;
@@ -38,6 +83,19 @@ export default defineComponent({
         CADASTRAR_TAREFA,
         tarefa
       );
+    },
+    selecionarTarefa(tarefa: ITarefa): void {
+      this.tarefaSelecionada = tarefa;
+    },
+    fecharModal(): void {
+      this.tarefaSelecionada = null;
+    },
+    alterarTarefa(): void {
+      (this.store as ReturnType<typeof useStore>)
+        .dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => {
+          this.fecharModal();
+        });
     },
   },
   setup() {
